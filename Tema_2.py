e8a1074689f54a708d13d28567337f0cc9858d33
xaviersaez <126728159+xaviersaez@users.noreply.github.com>
2023-05-19 20:43:41 -0300

    Add files via upload

---
+++ Tema_2.py
@@ -0,0 +1,41 @@
+# Parcial de programación Python
+
+# El parcial consta de 3 ejercicios, los cuales deben cumplir con la consigna para poder ser aprobados, aunque llegado el caso puntual que no se complete en su totalidad se dará la oportunidad de defender la logica aplicada de forma verbal en la proxima clase.
+
+# Se deberá entregar el parcial eligiendo algunas de las siguientes opciones:
+# 1- Enviando el link de su repositorio
+# 2- Enviar los archivos al correo lozanorichard32@gmail.com
+
+#Nombre Completo: xavier saez
+
+# ------------------------------ EJERCICIOS -----------------------------------------
+
+# 1- contador_vocales: Escribir una funciona que reciba una cadena de texto por teclado y que cuente 
+# la cantidad de vocales que contiene.
+#Ejemplo: cadena = "Hoy es el parcial"    ==> Esto me devolvera que en la cadena hay 6 vocales
+
+#palabra = input("ingrese una palabra: ")
+#print(len(palabra))
+#print(palabra.count("a"))
+#print(palabra.count("e"))
+#print(palabra.counte("i"))
+#print(palabra.counte("o"))
+#print(palabra.counte("u"))
+
+# 2- area_triangulo: Escribir una funcion que tome por teclado la base y altura de un triangulo y 
+# calcule su aréa. La funcion debe retornar el resultado.
+
+#base = int(input("ponga la base del triangulo: "))
+#altura = int(input("ponga la altura del triangulo: "))
+#def area_triangulo(base, altura):
+#    return altura * (base / 2)
+#resultado = area_triangulo(base, altura)
+#print(resultado)
+
+# 3- FizzBuzz: Escribir un programa que imprima los numeros del 1 al 100, pero que para los multiplos de 
+# 3 imprima "Fizz" en lugar del numero, para los multiplos de 5 imprima "Buzz" en lugar del numero y para los 
+# multiplos de 3 y 5 imprima "FizzBuzz"
+#Ayuda: Multiplos: Es el resultado de multiplicar un número por todos y cada uno de los numeros naturales
+
+
+
